ab36cec8bd4b5e390eddfb40452852ec1a6a2f07
alysebos <alyse.bos@gmail.com>
2019-05-16 10:37:40 -0700

    set up the server.js file, added the test files, and added a static html page to serve when users get the root

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("expresS");
+const morgan = require("morgan");
+
+const app = express();
+app.use(express.json());
+
+app.use(morgan("common"));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+//app.use("/pets", require("./petsRouter"));
+//app.use("/users", require("./usersRouter"));
+//app.use("/checkups", require("./checkupsRouter"));
+
+let server;
+
+function runServer() {
+	const port = process.env.PORT || 8080;
+	return new Promise((resolve, reject) => {
+		server = app
+			.listen(port, () => {
+				console.log(`Your app is listening on port ${port}`);
+				resolve(server);
+			})
+			.on("error", err => {
+				reject(err);
+			});
+	});
+}
+
+function closeServer() {
+	return new Promise((resolve, reject) => {
+		console.log("Closing server");
+		server.close(err => {
+			if(err) {
+				reject(err);
+				return;
+			}
+			resolve();
+		});
+	});
+}
+
+if (require.main === module) {
+	runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
