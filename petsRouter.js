058405aa96aa62731e6e74d4363a53175f0573ba
alysebos <alyse.bos@gmail.com>
2019-05-18 09:33:57 -0700

    added way to users to specify email for owner when adding pets

--- petsRouter.js
+++ petsRouter.js
@@ -3,19 +3,35 @@ const router = express.Router();
 
 const { Pets, Users } = require("./models");
 
-// LATER ON, ONCE USERS ARE ACTUALLY WORKING, 
-// PETS OWNER WILL BE THE ID OF THE PERSON SIGNED IN
-// BUT FOR NOW IT IS JUST AN EMAIL
+// I TRIED TO MAKE IT CREATE A PET AT THE SAME
+// TIME THAT IT ADDS THE PET TO THE USER'S ARRAY
 
+Users.create("Alyse", "alyse.bos@gmail.com", "TurdTurd1", true);
+Users.create("Alex", "eqaddictedfool@gmail.com", "LookAtMe1", false);
+Users.create("Sarah", "sarahmariehamilton@gmail.com", "ImTheFavorite", true);
+Users.items[Users.get()[0].id].pets.push(Pets.create("Turd", "Cat", "American Shorthair", "April 1, 2009", "pounds", "https://i.imgur.com/AD3MbBi.jpg", Users.get()[0].id).id);
+Users.items[Users.get()[1].id].pets.push(Pets.create("Bean", "Cat", "American Longhair", "August 31, 2001", "pounds", "https://i.imgur.com/AD3MbBi.jpg", Users.get()[1].id).id);
 
-function createPets () {
-	Users.items[Users.get()[0].id].pets.push(Pets.create("Turd", "Cat", "American Shorthair", "April 1, 2009", "pounds", "https://i.imgur.com/AD3MbBi.jpg", Users.get()[0].id).id);
-	Users.items[Users.get()[1].id].pets.push(Pets.create("Bean", "Cat", "American Longhair", "August 31, 2001", "pounds", "https://i.imgur.com/AD3MbBi.jpg", Users.get()[1].id).id);
+function missingField(object, fields) {
+	for (let i = 0; i < fields.length; i++) {
+		const field = fields[i];
+		if (!(field in object)) {
+			return `Missing required field \`${field}\``;
+		};
+	};
+	return false;
 }
-// this timeout allows the pets to be created after the users
-setTimeout(createPets, 500);
 
+function isEmail(string) {
+	let regEx = /[\w-]+@([\w-]+\.)+[\w-]+/;
+	return regEx.test(string);
+}
 
+function findUserFromEmail(email) {
+	return Users.get().find(user => {
+		return user.email === email;
+	})
+}
 
 router.get("/", (req, res) => {
 	res.status(200).json(Pets.get());
@@ -33,29 +49,29 @@ router.get("/:id", (req, res) => {
 router.post("/", (req, res) => {
 	// CHECK IF REQUIRED FIELDS ARE IN THE REQUEST
 	const requiredFields = ["name", "species", "breed", "weightUnits", "birthDate", "owner"];
-	for (let i = 0; i < requiredFields.length; i++) {
-		const field = requiredFields[i];
-		if (!(field in req.body)) {
-			const message = `Missing required field \`${field}\``;
-			console.error(message);
-			return res.status(400).send(message);
-		};
-	};
+	let message = missingField(req.body, requiredFields);
+	if (message) {
+		console.error(message);
+		return res.status(400).send(message);
+	}
+	// IF THE OWNER IS AN EMAIL, GET THE ID
+	if (isEmail(req.body.owner)) {
+		req.body.owner = findUserFromEmail(req.body.owner).id;
+	}
 	// MAKE SURE THE OWNER EXISTS
-	if (!Users.items[req.body.owner]) {
+	else if (!Users.items[req.body.owner]) {
 		const message = `\`${req.body.owner}\` cannot care for \`${req.body.name}\` because they do not exist`;
 		console.error(message);
 		return res.status(400).send(message);
 	};
 	// IF VERIFICATIONS PASS, CREATE THE PET
-	const item = Pets.create(req.body.name, req.body.species, req.body.breed, req.body.birthDate, req.body.weightUnits, req.body.photoUrl || false, req.body.owner);
+	const pet = Pets.create(req.body.name, req.body.species, req.body.breed, req.body.birthDate, req.body.weightUnits, req.body.photoUrl || false, req.body.owner);
 	// NOW WE HAVE TO ADD THE PET ID TO THE USER'S PETS
-	Users.items[req.body.owner].pets.push(item.id);
-	res.status(201).json(item);
+	Users.items[req.body.owner].pets.push(pet.id);
+	res.status(201).json(pet);
 });
 
 router.put("/:id", (req, res) => {
-
 });
 
 router.delete("/:id", (req, res) => {
--- usersRouter.js
+++ usersRouter.js
@@ -3,10 +3,6 @@ const router = express.Router();
 
 const { Users } = require("./models");
 
-Users.create("Alyse", "alyse.bos@gmail.com", "TurdTurd1", true);
-Users.create("Alex", "eqaddictedfool@gmail.com", "LookAtMe1", false);
-Users.create("Sarah", "sarahmariehamilton@gmail.com", "ImTheFavorite", true);
-
 router.get("/", (req, res) => {
 	res.status(200).json(Users.get());
 	// RIGHT NOW THIS DISPLAYS PASSWORDS.
